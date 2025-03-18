93443118aa204b06beb2e4ffae99a068b50e317e
emillysambas <samambaia.sambas2006@gmail.com>
2025-03-18 15:21:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('form-contato').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Mensagem enviada com sucesso!');
+    // Aqui você pode adicionar a lógica para enviar o formulário, como uma requisição AJAX
+});
